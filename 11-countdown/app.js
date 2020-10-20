7e99a0c209b279136ed5c0fcd7331fa4619ed896
pnguyen2101 <pnguyen2101@gmail.com>
2020-10-20 18:42:02 -0400

    update countdown

--- 11-countdown/app.js
+++ 11-countdown/app.js
@@ -26,3 +26,16 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
+let futureDate =new Date(2020,10,24,17,30,0);
+//console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+let day = futureDate.getDay();
+day = weekdays[day];
+giveaway.textContent = `giveaway ends on ${day}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
